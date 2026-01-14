46c3e05cb6d683f03fec795e134893311f4f4f42
pizza-init <s.e.hmissi@gmail.com>
2026-01-14 00:46:28 -0500

    Release v1.0.6

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@
 #include "esp_heap_caps.h"
 
 // Firmware version
-#define FIRMWARE_VERSION "1.0.5"
+#define FIRMWARE_VERSION "1.0.6"
 #define GITHUB_REPO "OpenSurface/SonosESP"
 #define GITHUB_API_URL "https://api.github.com/repos/" GITHUB_REPO "/releases/latest"
 
