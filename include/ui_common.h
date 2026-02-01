375fb4874ab30e107f35d702a8c1c451e74908ba
pizza-init <s.e.hmissi@gmail.com>
2026-02-01 14:03:11 -0500

    v1.1.8

--- include/ui_common.h
+++ include/ui_common.h
@@ -19,7 +19,7 @@
 #define DEFAULT_WIFI_PASSWORD ""
 
 // Firmware version
-#define FIRMWARE_VERSION "1.1.7-nightly.c42d3a8"
+#define FIRMWARE_VERSION "1.1.8"
 #define GITHUB_REPO "OpenSurface/SonosESP"
 #define GITHUB_API_URL "https://api.github.com/repos/" GITHUB_REPO "/releases/latest"
 
