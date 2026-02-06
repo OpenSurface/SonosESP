1e76b872d867a750e26a18d01bc097e99dda43cc
pizza-init <s.e.hmissi@gmail.com>
2026-02-06 17:57:22 -0500

    v1.2.0: Synced Lyrics & Stability Improvements

--- include/ui_common.h
+++ include/ui_common.h
@@ -19,7 +19,7 @@
 #define DEFAULT_WIFI_PASSWORD ""
 
 // Firmware version
-#define FIRMWARE_VERSION "1.1.9-nightly.0860926"
+#define FIRMWARE_VERSION "1.2.0"
 #define GITHUB_REPO "OpenSurface/SonosESP"
 #define GITHUB_API_URL "https://api.github.com/repos/" GITHUB_REPO "/releases/latest"
 
